8250f7315e175c4b1204dd0ae378763bd3518227
zhouzhongtao <zhouzhongtaosoft@163.com>
2017-08-06 11:55:05 -0400

    Admin init

---
+++ app/Http/Controllers/Admin/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        return "index";
+    }
+    public function login()
+    {
+        return "login";
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function(){
+    Route::get('/', 'IndexController@index');
+    Route::get('login','IndexController@login');
+});
