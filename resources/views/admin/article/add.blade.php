42f3f0f3cb9927516b074890f6569c4ba6a45de1
zhouzhongtao <zhouzhongtaosoft@163.com>
2017-08-30 12:36:46 -0400

    调整上传样式

--- resources/views/admin/article/add.blade.php
+++ resources/views/admin/article/add.blade.php
@@ -67,21 +67,27 @@
                         <td>
                             <input type="text" class="lg" name="art_thumb">
                             <input id="file_upload" name="file_upload" type="file" multiple="true">
-                            <script src="{{asset('resources/org/uploadify/jquery.uploadify.min.js')}}" type="text/javascript"></script>
-                            <link rel="stylesheet" type="text/css" href="{{asset('resources/org/uploadify/uploadify.css')}}">
+                            <script src="{{asset('resources/ext/uploadify/jquery.uploadify.min.js')}}" type="text/javascript"></script>
+                            <link rel="stylesheet" type="text/css" href="{{asset('resources/ext/uploadify/uploadify.css')}}">
                             <script type="text/javascript">
                                 <?php $timestamp = time();?>
                                 $(function() {
                                     $('#file_upload').uploadify({
+                                        'buttonText' : '图片上传',
                                         'formData'     : {
                                             'timestamp' : '<?php echo $timestamp;?>',
                                             '_token'     : "{{csrf_token()}}"
                                         },
-                                        'swf'      : "{{asset('resources/org/uploadify/uploadify.swf')}}",
+                                        'swf'      : "{{asset('resources/ext/uploadify/uploadify.swf')}}",
                                         'uploader' : "{{url('admin/upload')}}"
                                     });
                                 });
                             </script>
+                            <style>
+                                .uploadify{display:inline-block;}
+                                .uploadify-button{border:none; border-radius:5px; margin-top:8px;}
+                                table.add_tab tr td span.uploadify-button-text{color: #FFF; margin:0;}
+                            </style>
                         </td>
                     </tr>
                     <tr>
