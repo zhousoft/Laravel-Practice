297e218687eb28d0f9701f479753b7691d8e27d3
zhouzhongtao <zhouzhongtaosoft@163.com>
2017-08-31 13:22:38 -0400

    上传图片显示缩略图成功

--- app/Http/Controllers/Admin/CommonController.php
+++ app/Http/Controllers/Admin/CommonController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Input;
 use App\Http\Requests;
 
 class CommonController extends Controller
 {
     public function upload()
     {
-    	echo "预留上传接口";
+    	$file = Input::file('Filedata');
+    	if($file -> isValid()){ //验证文件是否有效
+            $entension = $file -> getClientOriginalExtension(); //上传文件的后缀.
+            $newName = date('YmdHis').mt_rand(100,999).'.'.$entension;
+            //$realPath = $file->getRealPath();//临时文件绝对路径
+            $path = $file -> move(base_path().'/uploads',$newName);
+            $filepath = 'uploads/'.$newName;
+            return $filepath;
+        }
     }
 }
--- resources/views/admin/article/add.blade.php
+++ resources/views/admin/article/add.blade.php
@@ -79,7 +79,11 @@
                                             '_token'     : "{{csrf_token()}}"
                                         },
                                         'swf'      : "{{asset('resources/ext/uploadify/uploadify.swf')}}",
-                                        'uploader' : "{{url('admin/upload')}}"
+                                        'uploader' : "{{url('admin/upload')}}",
+                                        'onUploadSuccess' : function(file, data, response) {
+                                            $('input[name=art_thumb]').val(data);
+                                            $('#art_thumb_img').attr('src','/'+data);
+                                        }
                                     });
                                 });
                             </script>
@@ -88,6 +92,12 @@
                                 .uploadify-button{border:none; border-radius:5px; margin-top:8px;}
                                 table.add_tab tr td span.uploadify-button-text{color: #FFF; margin:0;}
                             </style>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <td>
+                            <img src="" alt="" id="art_thumb_img" style="max-width: 350px; max-height:100px;">
                         </td>
                     </tr>
                     <tr>
