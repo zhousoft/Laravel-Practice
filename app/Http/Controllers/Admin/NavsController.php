dac40e428640198ad673784e6517f8d23cfafcf8
zhouzhongtao <zhouzhongtaosoft@163.com>
2017-09-08 12:13:28 -0400

    后台导航列表展示

--- app/Http/Controllers/Admin/NavsController.php
+++ app/Http/Controllers/Admin/NavsController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Http\Model\Navs;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Validator;
 
 class NavsController extends Controller
 {
-    //
+    //get.admin/navs  全部导航列表
+    public function index()
+    {
+        $data = Navs::orderBy('nav_order','asc')->get();
+        return view('admin.navs.index',compact('data'));
+    }
+    //get.admin/navs/{nav}  显示单个导航信息
+    public function show()
+    {
+
+    }
 }
