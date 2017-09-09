c712c68e3c31efab3f7f1c2a2b5829133b36272a
zhouzhongtao <zhouzhongtaosoft@163.com>
2017-09-09 13:09:08 -0400

    添加导航功能

--- app/Http/Controllers/Admin/NavsController.php
+++ app/Http/Controllers/Admin/NavsController.php
@@ -23,4 +23,38 @@ class NavsController extends Controller
     {
 
     }
+
+    //get.admin/navs/create   添加导航链接
+    public function create()
+    {
+        return view('admin/navs/add');
+    }
+
+    //post.admin/navs  添加自定义导航提交
+    public function store()
+    {
+        $input = Input::except('_token');
+        $rules = [
+            'nav_name'=>'required',
+            'nav_url'=>'required',
+        ];
+
+        $message = [
+            'nav_name.required'=>'自定义导航名称不能为空！',
+            'nav_url.required'=>'自定义导航URL不能为空！',
+        ];
+
+        $validator = Validator::make($input,$rules,$message);
+
+        if($validator->passes()){
+            $ret = Navs::create($input);
+            if($ret){
+                return redirect('admin/navs');
+            }else{
+                return back()->with('errors','自定义导航失败，请稍后重试！');
+            }
+        }else{
+            return back()->withErrors($validator);
+        }
+    }
 }
