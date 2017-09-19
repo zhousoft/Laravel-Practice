ef419fa36ea59b3f1dbfd37ca3fe67f96ff8e522
zhouzhongtao <zhouzhongtaosoft@163.com>
2017-09-19 12:24:09 -0400

    首页数据填充

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.home')
 @section('info')
-    <title>后盾个人博客</title>
-    <meta name="keywords" content="个人博客模板,博客模板" />
-    <meta name="description" content="寻梦主题的个人博客模板，优雅、稳重、大气,低调。" />
+    <title>{{Config::get('web.web_title')}} - {{Config::get('web.seo_title')}}</title>
+    <meta name="keywords" content="{{Config::get('web.keywords')}}" />
+    <meta name="description" content="{{Config::get('web.description')}}" />
 @endsection
+
 @section('content')
     <div class="banner">
         <section class="box">
@@ -18,15 +19,12 @@
     <div class="template">
         <div class="box">
             <h3>
-                <p><span>个人博客</span>模板 Templates</p>
+                <p><span>站长</span>推荐 Recommend</p>
             </h3>
             <ul>
-                <li><a href="/"  target="_blank"><img src="images/01.jpg"></a><span>仿新浪博客风格·梅——古典个人博客模板</span></li>
-                <li><a href="/" target="_blank"><img src="images/02.jpg"></a><span>黑色质感时间轴html5个人博客模板</span></li>
-                <li><a href="/"  target="_blank"><img src="images/03.jpg"></a><span>Green绿色小清新的夏天-个人博客模板</span></li>
-                <li><a href="/" target="_blank"><img src="images/04.jpg"></a><span>女生清新个人博客网站模板</span></li>
-                <li><a href="/"  target="_blank"><img src="images/02.jpg"></a><span>黑色质感时间轴html5个人博客模板</span></li>
-                <li><a href="/"  target="_blank"><img src="images/03.jpg"></a><span>Green绿色小清新的夏天-个人博客模板</span></li>
+                @foreach($pics as $p)
+                <li><a href="{{url('a/'.$p->art_id)}}"  target="_blank"><img src="{{url($p->art_thumb)}}"></a><span>{{$p->art_title}}</span></li>
+                @endforeach
             </ul>
         </div>
     </div>
@@ -35,76 +33,20 @@
             <p>文章<span>推荐</span></p>
         </h2>
         <div class="bloglist left">
-            <h3>程序员请放下你的技术情节，与你的同伴一起进步</h3>
-            <figure><img src="images/001.png"></figure>
+            @foreach($data as $d)
+            <h3>{{$d->art_title}}</h3>
+            <figure><img src="{{url($d->art_thumb)}}"></figure>
             <ul>
-                <p>如果说掌握一门赖以生计的技术是技术人员要学会的第一课的话， 那么我觉得技术人员要真正学会的第二课，不是技术，而是业务、交流与协作，学会关心其他工作伙伴的工作情况和进展...</p>
-                <a title="/" href="/" target="_blank" class="readmore">阅读全文>></a>
+                <p>{{$d->art_description}}</p>
+                <a title="{{$d->art_title}}" href="{{url('a/'.$d->art_id)}}" target="_blank" class="readmore">阅读全文>></a>
             </ul>
-            <p class="dateview"><span>2013-11-04</span><span>作者：后盾</span><span>个人博客：[<a href="/news/life/">程序人生</a>]</span></p>
-            <h3>程序员请放下你的技术情节，与你的同伴一起进步</h3>
-            <figure><img src="images/001.png"></figure>
-            <ul>
-                <p>如果说掌握一门赖以生计的技术是技术人员要学会的第一课的话， 那么我觉得技术人员要真正学会的第二课，不是技术，而是业务、交流与协作，学会关心其他工作伙伴的工作情况和进展...</p>
-                <a title="/" href="/" target="_blank" class="readmore">阅读全文>></a>
-            </ul>
-            <p class="dateview"><span>2013-11-04</span><span>作者：后盾</span><span>个人博客：[<a href="/news/life/">程序人生</a>]</span></p>
-            <h3>程序员请放下你的技术情节，与你的同伴一起进步</h3>
-            <figure><img src="images/001.png"></figure>
-            <ul>
-                <p>如果说掌握一门赖以生计的技术是技术人员要学会的第一课的话， 那么我觉得技术人员要真正学会的第二课，不是技术，而是业务、交流与协作，学会关心其他工作伙伴的工作情况和进展...</p>
-                <a title="/" href="/" target="_blank" class="readmore">阅读全文>></a>
-            </ul>
-            <p class="dateview"><span>2013-11-04</span><span>作者：后盾</span><span>个人博客：[<a href="/news/life/">程序人生</a>]</span></p>
-            <h3>程序员请放下你的技术情节，与你的同伴一起进步</h3>
-            <figure><img src="images/001.png"></figure>
-            <ul>
-                <p>如果说掌握一门赖以生计的技术是技术人员要学会的第一课的话， 那么我觉得技术人员要真正学会的第二课，不是技术，而是业务、交流与协作，学会关心其他工作伙伴的工作情况和进展...</p>
-                <a title="/" href="/" target="_blank" class="readmore">阅读全文>></a>
-            </ul>
-            <p class="dateview"><span>2013-11-04</span><span>作者：后盾</span><span>个人博客：[<a href="/news/life/">程序人生</a>]</span></p>
-            <h3>程序员请放下你的技术情节，与你的同伴一起进步</h3>
-            <figure><img src="images/001.png"></figure>
-            <ul>
-                <p>如果说掌握一门赖以生计的技术是技术人员要学会的第一课的话， 那么我觉得技术人员要真正学会的第二课，不是技术，而是业务、交流与协作，学会关心其他工作伙伴的工作情况和进展...</p>
-                <a title="/" href="/" target="_blank" class="readmore">阅读全文>></a>
-            </ul>
-            <p class="dateview"><span>2013-11-04</span><span>作者：后盾</span><span>个人博客：[<a href="/news/life/">程序人生</a>]</span></p>
+            <p class="dateview"><span>{{date('Y-m-d',$d->art_time)}}</span><span>作者：{{$d->art_editor}}</span></p>
+            @endforeach
+            <div class="page">
+                {{$data->links()}}
+            </div>
         </div>
         <aside class="right">
-            <div class="weather"><iframe width="250" scrolling="no" height="60" frameborder="0" allowtransparency="true" src="http://i.tianqi.com/index.php?c=code&id=12&icon=1&num=1"></iframe></div>
-            <div class="news">
-                <h3>
-                    <p>最新<span>文章</span></p>
-                </h3>
-                <ul class="rank">
-                    <li><a href="/" title="Column 三栏布局 个人网站模板" target="_blank">Column 三栏布局 个人网站模板</a></li>
-                    <li><a href="/" title="with love for you 个人网站模板" target="_blank">with love for you 个人网站模板</a></li>
-                    <li><a href="/" title="免费收录网站搜索引擎登录口大全" target="_blank">免费收录网站搜索引擎登录口大全</a></li>
-                    <li><a href="/" title="做网站到底需要什么?" target="_blank">做网站到底需要什么?</a></li>
-                    <li><a href="/" title="企业做网站具体流程步骤" target="_blank">企业做网站具体流程步骤</a></li>
-                    <li><a href="/" title="建站流程篇——教你如何快速学会做网站" target="_blank">建站流程篇——教你如何快速学会做网站</a></li>
-                    <li><a href="/" title="box-shadow 阴影右下脚折边效果" target="_blank">box-shadow 阴影右下脚折边效果</a></li>
-                    <li><a href="/" title="打雷时室内、户外应该需要注意什么" target="_blank">打雷时室内、户外应该需要注意什么</a></li>
-                </ul>
-                <h3 class="ph">
-                    <p>点击<span>排行</span></p>
-                </h3>
-                <ul class="paih">
-                    <li><a href="/" title="Column 三栏布局 个人网站模板" target="_blank">Column 三栏布局 个人网站模板</a></li>
-                    <li><a href="/" title="withlove for you 个人网站模板" target="_blank">with love for you 个人网站模板</a></li>
-                    <li><a href="/" title="免费收录网站搜索引擎登录口大全" target="_blank">免费收录网站搜索引擎登录口大全</a></li>
-                    <li><a href="/" title="做网站到底需要什么?" target="_blank">做网站到底需要什么?</a></li>
-                    <li><a href="/" title="企业做网站具体流程步骤" target="_blank">企业做网站具体流程步骤</a></li>
-                </ul>
-                <h3 class="links">
-                    <p>友情<span>链接</span></p>
-                </h3>
-                <ul class="website">
-                    <li><a href="http://www.houdunwang.com">后盾网</a></li>
-                    <li><a href="http://bbs.houdunwang.com">后盾论坛</a></li>
-                </ul>
-            </div>
             <!-- Baidu Button BEGIN -->
             <div id="bdshare" class="bdshare_t bds_tools_32 get-codes-bdshare"><a class="bds_tsina"></a><a class="bds_qzone"></a><a class="bds_tqq"></a><a class="bds_renren"></a><span class="bds_more"></span><a class="shareCount"></a></div>
             <script type="text/javascript" id="bdshare_js" data="type=tools&amp;uid=6574585" ></script>
@@ -113,6 +55,34 @@
                 document.getElementById("bdshell_js").src = "http://bdimg.share.baidu.com/static/js/shell_v2.js?cdnversion=" + Math.ceil(new Date()/3600000)
             </script>
             <!-- Baidu Button END -->
+
+            <div class="news" style="float:left;">
+                <h3>
+                    <p>最新<span>文章</span></p>
+                </h3>
+                <ul class="rank">
+                    @foreach($new as $n)
+                    <li><a href="{{url('a/'.$n->art_id)}}" title="{{$n->art_title}}" target="_blank">{{$n->art_title}}</a></li>
+                    @endforeach
+                </ul>
+                <h3 class="ph">
+                    <p>点击<span>排行</span></p>
+                </h3>
+                <ul class="paih">
+                    @foreach($hot as $h)
+                    <li><a href="{{url('a/'.$h->art_id)}}" title="{{$h->art_title}}" target="_blank">{{$h->art_title}}</a></li>
+                    @endforeach
+                </ul>
+                <h3 class="links">
+                    <p>友情<span>链接</span></p>
+                </h3>
+                <ul class="website">
+                    @foreach($links as $l)
+                    <li><a href="{{$l->link_url}}" target="_blank">{{$l->link_name}}</a></li>
+                    @endforeach
+                </ul>
+            </div>
+
         </aside>
     </article>
 @endsection
