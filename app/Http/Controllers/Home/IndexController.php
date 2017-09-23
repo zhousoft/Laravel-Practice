92b8e52844408ec79af3248af6040f91f279a1fb
zhouzhongtao <zhouzhongtaosoft@163.com>
2017-09-23 14:24:19 -0400

    调整格式

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -10,36 +10,36 @@ class IndexController extends CommonController
     public function index()
     {
         //点击量最高的6篇文章（站长推荐）
-        $pics = Article::orderBy('art_view','desc')->take(6)->get();
+        $pics = Article::orderBy('art_view', 'desc')->take(6)->get();
 
         //点击量最高的6篇文章
-        $hot = Article::orderBy('art_view','desc')->take(5)->get();
+        $hot = Article::orderBy('art_view', 'desc')->take(5)->get();
 
         //最新发布文章8篇
-        $new = Article::orderBy('art_time','desc')->take(8)->get();
+        $new = Article::orderBy('art_time', 'desc')->take(8)->get();
 
         //图文列表5篇（带分页）
-        $data = Article::orderBy('art_time','desc')->paginate(5);
+        $data = Article::orderBy('art_time', 'desc')->paginate(5);
 
         //友情链接
-        $links = Links::orderBy('link_order','asc')->get();
+        $links = Links::orderBy('link_order', 'asc')->get();
 
-        return view('home.index',compact('hot','new','pics','data','links'));
+        return view('home.index', compact('hot', 'new', 'pics', 'data', 'links'));
     }
 
     public function cate($cate_id)
     {
         //图文列表4篇（带分页）
-        $data = Article::where('cate_id',$cate_id)->orderBy('art_time','desc')->paginate(4);
+        $data = Article::where('cate_id', $cate_id)->orderBy('art_time','desc')->paginate(4);
 
         //查看次数自增
-        Category::where('cate_id',$cate_id)->increment('cate_view');
+        Category::where('cate_id', $cate_id)->increment('cate_view');
 
         //当前分类的子分类
-        $submenu = Category::where('cate_pid',$cate_id)->get();
+        $submenu = Category::where('cate_pid', $cate_id)->get();
 
         $field = Category::find($cate_id);
-        return view('home.list',compact('field','data','submenu'));
+        return view('home.list', compact('field', 'data', 'submenu'));
     }
 
     public function article()
